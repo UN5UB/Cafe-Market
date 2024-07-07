98e5cff26aa2dd2818753ff9f5b806fe96e80c97
Anton <antonsoluha@gmail.com>
2024-07-07 20:56:17 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ function App() {
         ) : null}
         <Header onCart={() => setCardOpened(true)} />
         <Routes>
-          <Route path="/" element={<Home onAddToCart={onAddToCart} />} exact />
+          <Route path="/" element={<Home onAddToCart={onAddToCart} />} />
           <Route path="products" element={<Menu onAddToCart={onAddToCart} />} />
           <Route path="about" element={<AboutPage />} />
           <Route path="delivery" element={<DeliveryPage />} />
