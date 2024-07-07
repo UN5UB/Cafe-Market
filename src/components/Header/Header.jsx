0360656ed072d106ad954ec1163d2f034e6fcff5
Anton <antonsoluha@gmail.com>
2024-07-07 20:51:55 +0300

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = ({ onCart }) => {
 
   return (
     <header className={styles.header}>
-      <Link to="" className={styles.logo}>
+      <Link to="/" className={styles.logo}>
         <img src="Icons/logo_coffe.svg" alt="Cafe Street" />
       </Link>
       <nav>
