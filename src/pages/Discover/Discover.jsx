8dc28ec41908f85097e58df53f10eb6292bb88c1
Anton <antonsoluha@gmail.com>
2024-07-08 10:20:10 +0300

    fix

--- src/pages/Discover/Discover.jsx
+++ src/pages/Discover/Discover.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./Discover.module.scss";
-import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 export default function Discover() {
   return (
